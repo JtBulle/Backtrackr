4bcbc77e90f2b2f17dc39b6f194c29b54be1fb36
JtBulle <julian.stelmakh@gmail.com>
2025-08-02 17:19:49 +0300

    Minor functionality tweaks

--- src/content-script.js
+++ src/content-script.js
@@ -3,12 +3,29 @@ const getTitle = () => {
   return document.title || (h1 && h1.innerText.trim() ? h1.innerText.trim() : "(no title)");
 };
 
-const domain = new URL(location.href).hostname;
+const sendPageInfo = () => {
+  const domain = new URL(location.href).hostname;
+  const title = `${getTitle()} - ${domain}`;
 
-const title = `${getTitle()} - ${domain}`;
+  chrome.runtime.sendMessage({
+    type: "NEW_LINK",
+    url: location.href,
+    title: title,
+  });
+};
 
-chrome.runtime.sendMessage({
-  type: "NEW_LINK",
-  url: location.href,
-  title: title,
+let lastUrl = location.href;
+
+sendPageInfo();
+
+const observer = new MutationObserver(() => {
+  if (location.href !== lastUrl) {
+    lastUrl = location.href;
+    setTimeout(sendPageInfo, 2000);
+  }
+});
+
+observer.observe(document, {
+  subtree: true,
+  childList: true,
 });
